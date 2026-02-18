af8d806a37071327219ae4f13a28770678f1e0f2
Vansh <poseidonsharma2005@gmail.com>
2026-02-19 00:57:12 +0530

    Initial HealthOS project setup with FastAPI and PostgreSQL

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from app.api.router import api_router
+
+
+app = FastAPI(title=settings.APP_NAME)
+
+app.include_router(api_router)
+
+
+@app.get("/")
+def root():
+    return {"message": "HealthOS Backend Running"}
