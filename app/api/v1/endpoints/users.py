7f5a2769a27a329d75c73d69a2665fd631c98266
Vansh <poseidonsharma2005@gmail.com>
2026-02-19 01:23:22 +0530

    feat(user): add User model, schemas, and user creation endpoint with PostgreSQL integration

---
+++ app/api/v1/endpoints/users.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
+
+router = APIRouter()
+
+
+@router.post("/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password_hash=user.password  # TEMPORARY — will hash in Day 3
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
