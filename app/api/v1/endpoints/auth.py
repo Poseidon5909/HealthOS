1a9b9470c0e34a515bdecbc7dff4a52d83c935df
Vansh <poseidonsharma2005@gmail.com>
2026-02-20 23:08:52 +0530

    feat(auth): implement Argon2 password hashing, JWT authentication with OAuth2, login endpoint, and protected routes

---
+++ app/api/v1/endpoints/auth.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.schemas.user import UserLogin
+from app.services.auth_service import authenticate_user
+from app.core.security import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import status
+router = APIRouter()
+
+
+@router.post("/login")
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    token = authenticate_user(
+        db,
+        email=form_data.username,   # OAuth2 uses "username"
+        password=form_data.password
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
+@router.get("/protected")
+def protected_route(current_user=Depends(get_current_user)):
+    return {"message": f"Welcome {current_user.name}"}
